809b4c81b3705c6a2100cb98e2a531b43a4decc9
“franklgy23” <franklgy23@gmail.com>
2021-04-19 14:53:25 +0800

    修改configstore无文件夹报错的bug

--- horoscope-core/src/main/scala/com/didichuxing/horoscope/service/storage/ZookeeperConfigStore.scala
+++ horoscope-core/src/main/scala/com/didichuxing/horoscope/service/storage/ZookeeperConfigStore.scala
@@ -166,6 +166,7 @@ class ZookeeperConfigStore(curator: CuratorFramework) extends ConfigStore with T
       path(Segment) { configType =>
         get {
           if (configType == LOG_TYPE || configType == SUBSCRIPTION_TYPE || configType == EXPERIMENT_TYPE) {
+            createFolderIfNotExist(configType)
             val contentList = getChildrenContent(configType)
             complete(HttpResponse(StatusCodes.OK, entity = contentList))
           } else {
