7d66c7b82315710bc5c368a4fab324f6f0d0327d
“franklgy23” <franklgy23@gmail.com>
2021-04-26 20:03:19 +0800

    map[string, string]

--- horoscope-analysis/src/main/scala/com/didichuxing/horoscope/ods/ProcedureViewBuilder.scala
+++ horoscope-analysis/src/main/scala/com/didichuxing/horoscope/ods/ProcedureViewBuilder.scala
@@ -57,7 +57,7 @@ final class ProcedureViewBuilder {
   private def buildResult(): Map[String, String] = {
     val assign = proc.getAssignMap.asScala.mapValues(Value(_).toJson).toMap
     val composite = proc.getCompositeMap.asScala.mapValues(c => {
-      if (c == null) Value(None) else Value(c).toJson
+      if (c == null) Value(None).toJson else Value(c).toJson
     }).filter(x => x._2.toString.length > 2).toMap
     (assign ++ composite)
   }
